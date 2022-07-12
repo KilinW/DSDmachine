81036af66795d38a984098b1132e4b3e1e22edfe
DENNIS022 <denniskao2001@gmail.com>
2022-07-12 13:19:57 +0800

    Gcode_sender

--- pico/Gcode_sender.cpp
+++ pico/Gcode_sender.cpp
@@ -1,3 +1,13 @@
 #include "Gcode_sender.h"
 
 void Gcode_sender::send_gcode(char* gcode){};
+
+void moveTo(char* code_buffer, float x, float y){
+    sprintf(code_buffer, "G00X%fY%f", x, y); 
+    return;
+};
+
+void moveTo(char* code_buffer, float x, float y, float speed){
+    sprintf(code_buffer, "G01X%fY%fF%f", x, y ,v); 
+    return;
+};
--- pico/Gcode_sender.h
+++ pico/Gcode_sender.h
@@ -8,8 +8,9 @@ class Gcode_sender{
     public:
         Gcode_sender(int tx, int rx);
         void send_gcode(char* gcode);
-        bool moveTo(float x, float y);
-        bool moveTo(float x, float y, float speed);
+        void moveTo(char* code_buffer, float x, float y);
+        void moveTo(char* code_buffer, float x, float y, float speed);
+    
 };
 
 
