ce41b2201b83d01603d9580ea413c82e38a9f72e
DENNIS022 <denniskao2001@gmail.com>
2022-07-15 16:21:49 +0800

    Uart protoocl finished

--- pico/Gcode_sender.cpp
+++ pico/Gcode_sender.cpp
@@ -1,4 +1,5 @@
 #include "Gcode_sender.h"
+#include "string"
 
 void Gcode_sender::send_gcode(char* gcode){};
 
@@ -10,4 +11,17 @@ void moveTo(char* code_buffer, float x, float y){
 void moveTo(char* code_buffer, float x, float y, float speed){
     sprintf(code_buffer, "G01X%fY%fF%f", x, y, speed); 
     return;
+};
+
+void operate_pc_command(char* pcbuffer){
+    char* word = strtok(pcbuffer[1]," ");
+    
+    char* word = strtok(NULL," ");
+    float x = stof(word);
+
+    char* word = strtok(NULL," ");
+    float y = stof(word);
+
+    moveTo(pc_buffer, x, y);
+    
 };
